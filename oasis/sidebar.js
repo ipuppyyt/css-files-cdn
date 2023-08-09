f1564b344db1ba4af097d99ec5d1a3315ce66dda
iPuppyYT <dhanush6102002@gmail.com>
2023-08-09 20:08:01 +0530

    Update sidebar.js

--- oasis/sidebar.js
+++ oasis/sidebar.js
@@ -1,18 +1,28 @@
-function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typeof key === "symbol" ? key : String(key); }
-function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
-const ANIMATION_DURATION = 300;
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const ANIMATION_DURATION = 300;
+
 const SIDEBAR_EL = document.getElementById("sidebar");
-const SUB_MENU_ELS = document.querySelectorAll(".menu > ul > .menu-item.sub-menu");
-const FIRST_SUB_MENUS_BTN = document.querySelectorAll(".menu > ul > .menu-item.sub-menu > a");
-const INNER_SUB_MENUS_BTN = document.querySelectorAll(".menu > ul > .menu-item.sub-menu .menu-item.sub-menu > a");
+
+const SUB_MENU_ELS = document.querySelectorAll(
+".menu > ul > .menu-item.sub-menu");
+
+
+const FIRST_SUB_MENUS_BTN = document.querySelectorAll(
+".menu > ul > .menu-item.sub-menu > a");
+
+
+const INNER_SUB_MENUS_BTN = document.querySelectorAll(
+".menu > ul > .menu-item.sub-menu .menu-item.sub-menu > a");
+
+
 class PopperObject {
-  constructor(reference, popperTarget) {
-    _defineProperty(this, "instance", null);
-    _defineProperty(this, "reference", null);
-    _defineProperty(this, "popperTarget", null);
+
+
+
+
+  constructor(reference, popperTarget) {_defineProperty(this, "instance", null);_defineProperty(this, "reference", null);_defineProperty(this, "popperTarget", null);
     this.init(reference, popperTarget);
   }
+
   init(reference, popperTarget) {
     this.reference = reference;
     this.popperTarget = popperTarget;
@@ -20,64 +30,89 @@ class PopperObject {
       placement: "right",
       strategy: "fixed",
       resize: true,
-      modifiers: [{
+      modifiers: [
+      {
         name: "computeStyles",
         options: {
-          adaptive: false
-        }
-      }, {
+          adaptive: false } },
+
+
+      {
         name: "flip",
         options: {
-          fallbackPlacements: ["left", "right"]
-        }
-      }]
-    });
-    document.addEventListener("click", e => this.clicker(e, this.popperTarget, this.reference), false);
+          fallbackPlacements: ["left", "right"] } }] });
+
+
+
+
+
+    document.addEventListener(
+    "click",
+    e => this.clicker(e, this.popperTarget, this.reference),
+    false);
+
+
     const ro = new ResizeObserver(() => {
       this.instance.update();
     });
+
     ro.observe(this.popperTarget);
     ro.observe(this.reference);
   }
+
   clicker(event, popperTarget, reference) {
-    if (SIDEBAR_EL.classList.contains("collapsed") && !popperTarget.contains(event.target) && !reference.contains(event.target)) {
+    if (
+    SIDEBAR_EL.classList.contains("collapsed") &&
+    !popperTarget.contains(event.target) &&
+    !reference.contains(event.target))
+    {
       this.hide();
     }
   }
+
   hide() {
     this.instance.state.elements.popper.style.visibility = "hidden";
-  }
-}
+  }}
+
+
 class Poppers {
-  constructor() {
-    _defineProperty(this, "subMenuPoppers", []);
+
+
+  constructor() {_defineProperty(this, "subMenuPoppers", []);
     this.init();
   }
+
   init() {
     SUB_MENU_ELS.forEach(element => {
-      this.subMenuPoppers.push(new PopperObject(element, element.lastElementChild));
+      this.subMenuPoppers.push(
+      new PopperObject(element, element.lastElementChild));
+
       this.closePoppers();
     });
   }
+
   togglePopper(target) {
-    if (window.getComputedStyle(target).visibility === "hidden") target.style.visibility = "visible";else target.style.visibility = "hidden";
+    if (window.getComputedStyle(target).visibility === "hidden")
+    target.style.visibility = "visible";else
+    target.style.visibility = "hidden";
   }
+
   updatePoppers() {
     this.subMenuPoppers.forEach(element => {
       element.instance.state.elements.popper.style.display = "none";
       element.instance.update();
     });
   }
+
   closePoppers() {
     this.subMenuPoppers.forEach(element => {
       element.hide();
     });
-  }
-}
+  }}
+
+
 const slideUp = (target, duration = ANIMATION_DURATION) => {
-  const {
-    parentElement
-  } = target;
+  const { parentElement } = target;
   parentElement.classList.remove("open");
   target.style.transitionProperty = "height, margin, padding";
   target.style.transitionDuration = `${duration}ms`;
@@ -103,14 +138,10 @@ const slideUp = (target, duration = ANIMATION_DURATION) => {
   }, duration);
 };
 const slideDown = (target, duration = ANIMATION_DURATION) => {
-  const {
-    parentElement
-  } = target;
+  const { parentElement } = target;
   parentElement.classList.add("open");
   target.style.removeProperty("display");
-  let {
-    display
-  } = window.getComputedStyle(target);
+  let { display } = window.getComputedStyle(target);
   if (display === "none") display = "block";
   target.style.display = display;
   const height = target.offsetHeight;
@@ -136,10 +167,13 @@ const slideDown = (target, duration = ANIMATION_DURATION) => {
     target.style.removeProperty("transition-property");
   }, duration);
 };
+
 const slideToggle = (target, duration = ANIMATION_DURATION) => {
-  if (window.getComputedStyle(target).display === "none") return slideDown(target, duration);
+  if (window.getComputedStyle(target).display === "none")
+  return slideDown(target, duration);
   return slideUp(target, duration);
 };
+
 const PoppersInstance = new Poppers();
 
 /**
@@ -157,9 +191,11 @@ const updatePoppersTimeout = () => {
 document.getElementById("btn-collapse").addEventListener("click", () => {
   SIDEBAR_EL.classList.toggle("collapsed");
   PoppersInstance.closePoppers();
-  if (SIDEBAR_EL.classList.contains("collapsed")) FIRST_SUB_MENUS_BTN.forEach(element => {
+  if (SIDEBAR_EL.classList.contains("collapsed"))
+  FIRST_SUB_MENUS_BTN.forEach(element => {
     element.parentElement.classList.remove("open");
   });
+
   updatePoppersTimeout();
 });
 
@@ -168,6 +204,7 @@ document.getElementById("btn-collapse").addEventListener("click", () => {
  */
 document.getElementById("btn-toggle").addEventListener("click", () => {
   SIDEBAR_EL.classList.toggle("toggled");
+
   updatePoppersTimeout();
 });
 
@@ -177,7 +214,9 @@ document.getElementById("btn-toggle").addEventListener("click", () => {
 document.getElementById("overlay").addEventListener("click", () => {
   SIDEBAR_EL.classList.toggle("toggled");
 });
+
 const defaultOpenMenus = document.querySelectorAll(".menu-item.sub-menu.open");
+
 defaultOpenMenus.forEach(element => {
   element.lastElementChild.style.display = "block";
 });
@@ -187,9 +226,18 @@ defaultOpenMenus.forEach(element => {
  */
 FIRST_SUB_MENUS_BTN.forEach(element => {
   element.addEventListener("click", () => {
-    if (SIDEBAR_EL.classList.contains("collapsed")) PoppersInstance.togglePopper(element.nextElementSibling);else {
+    if (SIDEBAR_EL.classList.contains("collapsed"))
+    PoppersInstance.togglePopper(element.nextElementSibling);else
+    {
       const parentMenu = element.closest(".menu.open-current-submenu");
-      if (parentMenu) parentMenu.querySelectorAll(":scope > ul > .menu-item.sub-menu > a").forEach(el => window.getComputedStyle(el.nextElementSibling).display !== "none" && slideUp(el.nextElementSibling));
+      if (parentMenu)
+      parentMenu.
+      querySelectorAll(":scope > ul > .menu-item.sub-menu > a").
+      forEach(
+      (el) =>
+      window.getComputedStyle(el.nextElementSibling).display !==
+      "none" && slideUp(el.nextElementSibling));
+
       slideToggle(element.nextElementSibling);
     }
   });
